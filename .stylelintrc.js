5faf8a832e79eacb4b081d0201dfb9f9936f31cb
chen nan <15602964779@163.com>
2019-04-06 21:49:56 +0800

    增加stylelint检测css代码

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // https://github.com/stylelint/stylelint-config-standard
+  "extends": "stylelint-config-standard",
+  "rules": {
+    "string-quotes": "single",
+    "at-rule-empty-line-before": null,
+    "rule-empty-line-before": null,
+    "comment-empty-line-before": null
+  }
+}
